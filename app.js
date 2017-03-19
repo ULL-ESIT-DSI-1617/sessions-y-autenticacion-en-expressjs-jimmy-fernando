93a39953e2b9c02a2c0c498e8a689279342a540c
Fernando Nantes-Machado <fernandonantesmachadoytoledo@gmail.com>
2017-03-19 16:26:07 +0000

    Funciones de login creadas y vista de cambio de contraseña creada.

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var
     express = require('express'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
+    bcrypt = require('bcrypt-nodejs'),
+    jsonfile = require('jsonfile'),
 
-    users = require('./users.json'),
+    file = "./users.json",
 
     app = express(),
     port= process.env.PORT || 8080;
@@ -13,6 +15,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({extended: true})); //form params in req.body
+
+//Variable que almacenara el par de nombre de usuario y contraseña.
+var hash = bcrypt.hashSync("contraseña");
+console.log('contraseña de usuario encriptada: ${hash}');
+var users = {
+  usuario : hash,
+  fer : bcrypt.hashSync("ferpassword"),
+  jimmy : bcrypt.hashSync("jimmypassword"),
+  amy : bcrypt.hashSync("amyspassword")
+};
+
+//Escribimos esta información en el fichero users.json
+jsonfile.writeFile(file, users, {spaces: 2}, (err)=>{
+  console.error(err);
+});
+
 app.use(session({
     secret: '2C44-4D44-WppQ212',
     resave: true,
@@ -21,18 +39,46 @@ app.use(session({
 
 app.use(express.static('public'));
 
-//Falta autenticacion
-app.use('/book',express.static('book')); //acceder al libro en la ruta /book //gitbook build docs book
+//Cuando accedamos a la raíz, nos redireccionará al login.
+app.get('/', (req, res)=>{
+  res.redirect('/login');
+});
 
-app.get('/',(req,res)=>{
+app.get('/login',(req,res)=>{
     res.render('index');
 });
 
-app.post('/login',(req,res)=>{
-    //Implementar autenticacion y creacion de sesion
+//Cuando enviemos el formulario, se ejecutará este middleware.
+app.post('/login', (req, res)=>{
+  if(!req.body.username || !req.body.password){
+    console.log('login failed');
+    res.send('Login failed');
+  } else if(req.body.username in users &&
+            bcrypt.compareSync(req.body.password, users[req.body.username])){
+              req.session.user = req.body.username;
+              req.session.admin = true;
+              res.send("Login exitoso! usuario: "+req.session.user);
+            }else {
+              console.log('login ${util.inspect(req.query)} fallido');
+              res.send("No has podido loguear.");
+            }
 });
+
+//Esta función comprueba que el usuario esté autenticado.
+var auth = function(req, res, next) {
+  if (req.session && req.session.user in users)
+    return next();
+  else
+    return res.sendStatus(401);
+};
+
+//Cuando accedamos a la ruta "/content" y estemos autenticados, podremos leer el libro.
+app.use( "/content", [ auth, express.static( __dirname + "/book" ) ] );
+
+//Cuando accedamos a /logout, se nos eliminará la sesion.
 app.get('/logout',(req,res) =>{
-    //Implementar eliminacion de sesion
+  req.session.destroy();
+  res.send("logout exitoso!");
 })
 
 app.listen(port, (err)=>{
